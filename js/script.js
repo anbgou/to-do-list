0a89e42d3be78eb7f0be60bb0aaed3c0de0c9c6a
anbgou <gerdunjapes@gmail.com>
2024-04-21 19:34:00 +0300

    Almost done

--- js/script.js
+++ js/script.js
@@ -1,12 +1,88 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleSwitch = document.querySelector('#dark-mode-toggle');
-    const app = document.querySelector('.app');
+const addBtn = document.querySelector("#add-btn");
+const newTaskInput = document.querySelector("#wrapper input");
+const tasksContainer = document.querySelector("#tasks");
+const error = document.getElementById("error");
+const countValue = document.querySelector(".count-value");
+let taskCount = 0;
+const displayCount = (taskCount) => {
+    countValue.innerText = taskCount;
+};
 
-    toggleSwitch.addEventListener('change', () => {
-        if (toggleSwitch.checked) {
-            app.classList.add('dark-mode');
-        } else {
-            app.classList.remove('dark-mode');
-        }
+const addTask = () => {
+    const taskName = newTaskInput.value.trim();
+    error.style.display = 'none';
+    if(!taskName) {
+        setTimeout(() => {
+            error.style.display = 'block';
+        }, 200);
+        return;
+    }
+    const task = `<div class="task">
+        <input type="checkbox" class = "task-check">
+        <span class = "taskname">${taskName}</span>
+        <button class = "edit">
+        <i class="fa-regular fa-pen-to-square"></i>
+        </button>
+        <button class = "delete">
+        <i class="fa-regular fa-trash-can"></i>
+        </button>
+    </div>`;
+
+    tasksContainer.insertAdjacentHTML("beforeend", task);
+    const deleteButtons = document.querySelectorAll(".delete");
+    deleteButtons.forEach(button => {
+        button.onclick = () => {
+            button.parentNode.remove();
+            taskCount -= 1;
+            displayCount(taskCount);
+        };
     });
+
+    const editButtons = document.querySelectorAll(".edit");
+    editButtons.forEach((editBtn) => {
+        editBtn.onclick = (e) => {
+            let targetElement = e.target;
+            if(!(e.target.className == "edit")) {
+                targetElement = e.target.parentElement;
+            }
+            newTaskInput.value = targetElement.previousElementSibling?.innerText;
+            targetElement.parentNode.remove();
+            taskCount -= 1;
+            displayCount(taskCount);
+        };
+    });
+    const tasksCheck = document.querySelectorAll(".task-check");
+    tasksCheck.forEach((checkBox) => {
+        checkBox.onchange = () => {
+            checkBox.nextElementSibling.classList.toggle("completed");
+            if(checkBox.checked) {
+                taskCount -= 1;
+            } else {
+                taskCount += 1;
+            }
+            displayCount(taskCount);
+        };
+    });
+    taskCount += 1;
+    displayCount(taskCount);
+    newTaskInput.value = "";
+};
+
+addBtn.addEventListener("click", addTask);
+
+window.onload = () => {
+    taskCount = 0;
+    displayCount(taskCount);
+    newTaskInput.value = "";
+}
+
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('change', () => {
+  if (darkModeToggle.checked) {
+    body.classList.add('dark-mode');
+  } else {
+    body.classList.remove('dark-mode');
+  }
 });
